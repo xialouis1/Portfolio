050564d48d9863872f4bfed0542dbe04b5258fbb
Louis Xia <23242389+xialouis1@users.noreply.github.com>
2023-10-23 20:07:22 -0400

    Fetch Repos from GitHub API
    Fetch all the public repositories using GitHub API for user xialouis1 and display them to a React project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Header() {
+  return (
+    <div className="container-fluid text-center bg-secondary-subtle p-5">
+      <h1>Louis Xia</h1>
+      <h4>
+        Elmhurst, New York
+        &nbsp;|&nbsp;
+        <a class="text-decoration-none text-dark" href="mailto:xialouis1@gmail.com">
+          xialouis1@gmail.com
+        </a>
+        &nbsp;|&nbsp;
+        <a class="text-decoration-none text-dark" href="https://www.linkedin.com/in/xialouis1" target="_blank">
+          linkedin.com/in/xialouis1
+        </a>
+        &nbsp;|&nbsp;
+        <a class="text-decoration-none text-dark" href="https://github.com/xialouis1" target="_blank">
+          github.com/xialouis1
+        </a>
+      </h4>
+      <h4>Software Engineer | Team Lead | Veteran</h4>
+    </div>
+  );
+}
+
+function Projects() {
+  const [repos, setRepos] = useState(undefined);
+  const [error, setError] = useState(undefined);
+  
+  async function fetchRepos() {
+    //await new Promise(resolve=>setTimeout(resolve, 5000))
+    await fetch('https://api.github.com/users/xialouis1/repos')
+      .then(async response => 
+      {
+        if (response.status == 200) {
+          const array = await response.json();
+          array.sort((a, b) => a.id < b.id)
+          setRepos(array);
+          return;
+        } else {
+          setError(true);
+        }
+      })
+      .catch( () => { return undefined });
+  }
+
+  fetchRepos();
+
+  if(error) {
+    return <ProjectError/>
+  } else if (repos === undefined) {
+    return <ProjectLoading/>
+  } else {
+    return <>{repos.map((repo) => <Project title={repo.name} description={repo.description} link={repo.html_url}/>)}</>
+  }
+}
+
+function Project(props) {
+  return (
+    <div className="row my-5">
+      <div className="col-8 mx-auto bg-light">
+          <div className=""><h4><a href={props.link}>{props.title}</a></h4></div>
+          <div className="">{props.description}</div>
+      </div>
+    </div>
+  );
+}
+
+function ProjectLoading() {
+  return (
+    <div className="row my-5">
+      <div className="col-8 mx-auto text-center">
+        <span className="spinner-border align-middle" role="status"></span>
+        <span className="align-middle">&nbsp;Loading repositories from GitHub.</span>
+      </div>
+    </div>
+  );
+}
+
+function ProjectError() {
+  const projects = [
+    {
+      "name": "Name",
+      "description": "Description",
+      "html_url": ""
+    }, {
+      "name": "Name",
+      "description": "Description",
+      "html_url": ""
+    }
+  ];
+
+  return <>{projects.map((project) => <Project title={project.name} description={project.description} link={project.html_url}/>)}</>
+}
+
+function Portfolio() {
+  return (
+    <div className="container-fluid">
+      <Projects/>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Portfolio/>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
